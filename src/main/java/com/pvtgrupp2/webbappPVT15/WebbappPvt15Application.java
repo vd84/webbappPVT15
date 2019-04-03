f84667281bb0a5c00dda5b675ef159f63bb2ad76
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-03 15:20:46 +0200

    first commit

---
+++ src/main/java/com/pvtgrupp2/webbappPVT15/WebbappPvt15Application.java
@@ -0,0 +1,27 @@
+package com.pvtgrupp2.webbappPVT15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class WebbappPvt15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbappPvt15Application.class, args);
+	}
+
+}
+
+@Controller
+@SpringBootApplication
+class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/pvtgrupp2/webbappPVT15/WebbappPvt15ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pvtgrupp2.webbappPVT15;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebbappPvt15ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
