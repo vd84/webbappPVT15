844fbc1d029d29c1e90fd216c2688269400a6439
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-09 16:29:36 +0200

    Added hello

---
+++ src/main/java/WebbAppPVT15Grupp2/Repositories/HelloRespository.java
@@ -0,0 +1,7 @@
+package WebbAppPVT15Grupp2.Repositories;
+
+import WebbAppPVT15Grupp2.Models.Hello;
+import org.springframework.data.repository.CrudRepository;
+
+public interface HelloRespository extends CrudRepository<Hello, Integer> {
+}
