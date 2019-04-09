7d32578c433f6d3b9aa16a99d346d7919b960048
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-09 15:30:01 +0200

    changed name

--- src/main/java/WebbAppPVT15Grupp2/WebbappPvt15Application.java
+++ src/main/java/WebbAppPVT15Grupp2/WebbappPvt15Application.java
@@ -2,14 +2,23 @@ package WebbAppPVT15Grupp2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
+@Configuration
 public class WebbappPvt15Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebbappPvt15Application.class, args);
 
 
+	}
+	@RequestMapping("/")
+	String index() {
+		return "index";
 	}
 
 }
