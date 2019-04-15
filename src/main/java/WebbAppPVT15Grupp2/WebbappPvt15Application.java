68af79b6fa337674fc8e2f87aa3437ba7a16aecd
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-15 09:25:44 +0200

    added homepage

--- src/main/java/WebbAppPVT15Grupp2/WebbappPvt15Application.java
+++ src/main/java/WebbAppPVT15Grupp2/WebbappPvt15Application.java
@@ -2,13 +2,9 @@ package WebbAppPVT15Grupp2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
-@Configuration
 public class WebbappPvt15Application {
 
 	public static void main(String[] args) {
