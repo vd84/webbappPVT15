29147a350cf95bdebfb3b5d57c4c6983c2942075
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-16 10:04:40 +0200

    added homepage

--- src/test/java/com/pvtgrupp2/webbappPVT15/WebbappPvt15ApplicationTests.java
+++ src/test/java/com/pvtgrupp2/webbappPVT15/WebbappPvt15ApplicationTests.java
@@ -1,6 +1,5 @@
 package com.pvtgrupp2.webbappPVT15;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WebbappPvt15ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
