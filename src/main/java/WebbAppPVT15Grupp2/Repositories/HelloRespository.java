7396e2d5aef524ad452eadc3e9c37be2f05b9039
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-25 13:33:57 +0200

    Merge branch 'master' of https://github.com/vd84/webbappPVT15
    # Conflicts:
    #	src/main/java/WebbAppPVT15Grupp2/Controllers/UserController.java

--- src/main/java/WebbAppPVT15Grupp2/Repositories/HelloRespository.java
+++ src/main/java/WebbAppPVT15Grupp2/Repositories/HelloRespository.java
@@ -1,7 +1,9 @@
 package WebbAppPVT15Grupp2.Repositories;
 
 import WebbAppPVT15Grupp2.Models.Hello;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-public interface HelloRespository extends JpaRepository<Hello, Integer> {
+@CrossOrigin(origins = "http://localhost:4200")
+public interface HelloRespository extends CrudRepository<Hello, Integer> {
 }
