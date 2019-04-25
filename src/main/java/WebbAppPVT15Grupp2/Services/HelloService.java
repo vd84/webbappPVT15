9401d31bdd15b293562e3ebb38697427fcb70c1a
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-25 11:03:08 +0200

    Changed to false

--- src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
+++ src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
@@ -1,9 +1,11 @@
 package WebbAppPVT15Grupp2.Services;
 
-
 import WebbAppPVT15Grupp2.Models.Hello;
 import WebbAppPVT15Grupp2.Repositories.HelloRespository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,21 @@ public class HelloService {
     @Autowired
     HelloRespository respository;
 
+    public static final Logger logger = LoggerFactory.getLogger(HelloService.class);
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/hello")
     public Iterable<Hello> findAllHellos(){
         return respository.findAll();
     }
+
+
+
+
+
 }
+
+
+
+
+
