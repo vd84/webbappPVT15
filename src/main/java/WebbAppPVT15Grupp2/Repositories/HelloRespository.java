cbe61e543fef8d74f881eed879dbe3dc6d669be3
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-26 08:43:34 +0200

    Trying to make Api calls work

--- src/main/java/WebbAppPVT15Grupp2/Repositories/HelloRespository.java
+++ src/main/java/WebbAppPVT15Grupp2/Repositories/HelloRespository.java
@@ -2,8 +2,6 @@ package WebbAppPVT15Grupp2.Repositories;
 
 import WebbAppPVT15Grupp2.Models.Hello;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "*")
 public interface HelloRespository extends CrudRepository<Hello, Integer> {
 }
