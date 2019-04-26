70be031f5b98d46076773d0cc6e869cc5d8cd239
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-26 08:42:06 +0200

    Merge branch 'master' of https://github.com/vd84/webbappPVT15
    # Conflicts:
    #	src/main/java/WebbAppPVT15Grupp2/Controllers/UserController.java

--- src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
+++ src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
@@ -2,8 +2,6 @@ package WebbAppPVT15Grupp2.Services;
 
 import WebbAppPVT15Grupp2.Models.Hello;
 import WebbAppPVT15Grupp2.Repositories.HelloRespository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,24 +13,12 @@ public class HelloService {
     HelloRespository respository;
 
 
-    public static final Logger logger = LoggerFactory.getLogger(HelloService.class);
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/hello")
     public Iterable<Hello> findAllHellos() {
         return respository.findAll();
     }
 
-
-    //@RequestMapping(method = RequestMethod.POST)
-    @CrossOrigin(origins = "http://localhost:4")
-    @PostMapping("/addhello")
-    public String createHello() {
-
-        return "Hello from POST";
-
-
-    }
 
 
 }
