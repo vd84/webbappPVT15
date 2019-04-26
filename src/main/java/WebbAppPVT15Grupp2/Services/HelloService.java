f5d626b5d9f2fd471508abd3aa7f8750c929b5dc
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-26 08:42:34 +0200

    Revert "Changed to false"
    This reverts commit 94a406ef

--- src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
+++ src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
@@ -1,29 +1,21 @@
 package WebbAppPVT15Grupp2.Services;
 
+
 import WebbAppPVT15Grupp2.Models.Hello;
 import WebbAppPVT15Grupp2.Repositories.HelloRespository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path="/api")
 public class HelloService {
     @Autowired
     HelloRespository respository;
 
-
-
-    @GetMapping("/hello")
-    public Iterable<Hello> findAllHellos() {
+    @GetMapping("/getAllhello")
+    public Iterable<Hello> findAllHellos(){
         return respository.findAll();
     }
-
-
-
 }
-
-
-
-
-
