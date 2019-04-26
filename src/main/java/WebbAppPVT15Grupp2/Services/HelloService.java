7dc38015d97027c67e91d94704f7b1b84570120c
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-26 08:51:43 +0200

    Trying to make Api calls work

--- src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
+++ src/main/java/WebbAppPVT15Grupp2/Services/HelloService.java
@@ -4,6 +4,7 @@ package WebbAppPVT15Grupp2.Services;
 import WebbAppPVT15Grupp2.Models.Hello;
 import WebbAppPVT15Grupp2.Repositories.HelloRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class HelloService {
     @Autowired
     HelloRespository respository;
 
+    @CrossOrigin("http://localhost:4200/hello")
     @GetMapping("/hello")
     public Iterable<Hello> findAllHellos(){
         return respository.findAll();
