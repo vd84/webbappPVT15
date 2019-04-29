88f86c29e86a3e5b44bf3fa822f69a12f5258211
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-29 17:00:58 +0200

    Getting User by id

--- src/main/java/WebbAppPVT15Grupp2/Models/User.java
+++ src/main/java/WebbAppPVT15Grupp2/Models/User.java
@@ -8,7 +8,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private int id;
     private String username;
     private String password;
     private boolean active;
@@ -24,7 +24,7 @@ public class User {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
