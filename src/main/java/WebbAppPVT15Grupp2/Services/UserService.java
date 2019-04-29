19d56d80d74199d81478699b71a0fff14590ca8d
Douglas Hammarstam <dogge20@hotmail.com>
2019-04-29 16:08:02 +0200

    Getting User by id

--- src/main/java/WebbAppPVT15Grupp2/Services/UserService.java
+++ src/main/java/WebbAppPVT15Grupp2/Services/UserService.java
@@ -3,23 +3,40 @@ package WebbAppPVT15Grupp2.Services;
 
 import WebbAppPVT15Grupp2.Models.User;
 import WebbAppPVT15Grupp2.Repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path="/api")
+@RequestMapping(path = "/api")
 public class UserService {
     @Autowired
     UserRepository respository;
 
+    public static final Logger logger = LoggerFactory.getLogger(HelloService.class);
+
+
     @GetMapping("/user")
-    public Iterable<User> findAllUsers(){
+    public Iterable<User> findAllUsers() {
         return respository.findAll();
     }
 
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getUserById(@PathVariable("id") int id) {
+        logger.info("Fetching user by id {}", id);
+        User user = respository.findById(id).get();
 
+        if(user == null){
+            logger.error("User with id {} not found.", id);
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+
+
+    }
 
 
 }
