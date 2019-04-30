8cffaed47404c16d4974e7cf77fd52be083be335
peter.froberg@stenhamra.se <jtb10lNHVJ8>
2019-04-30 14:24:02 +0200

    adduser ws added

--- src/main/java/WebbAppPVT15Grupp2/Services/addUserService.java
+++ src/main/java/WebbAppPVT15Grupp2/Services/addUserService.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
+@CrossOrigin("*")
 public class addUserService {
     @Autowired
     PUser2Repository repo;
