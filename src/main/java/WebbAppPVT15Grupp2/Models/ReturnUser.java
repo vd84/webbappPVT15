2eee3fb67edde008d64c7b062b98c2abf9e61e80
peter.froberg@stenhamra.se <jtb10lNHVJ8>
2019-05-07 11:09:10 +0200

    Added WS for activity

--- src/main/java/WebbAppPVT15Grupp2/Models/ReturnUser.java
+++ src/main/java/WebbAppPVT15Grupp2/Models/ReturnUser.java
@@ -7,26 +7,35 @@ import javax.persistence.*;
 public class ReturnUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private long id;
-    @Column(name = "username")
+    //@Column(name = "id")
+    private int id;
+    //@Column(name = "username")
     private String username;
-    @Column(name = "password")
-    private String password;
-    @Column(name = "points")
+    //@Column(name = "points")
     private int points;
-    @Column(name = "fairplaypoints")
+    //@Column(name = "fairplaypoints")
     private int fairplaypoints;
-    @Column(name = "currentyouthcentre")
+    //@Column(name = "currentyouthcentre")
     private int currentyouthcentre;
-    @Column(name = "role")
+    private String currentyouthcentrename;
+    //@Column(name = "role")
     private int role;
 
-    public long getId() {
+    public ReturnUser(int id, String userName, int points, int fairPoints, int youthCentre, String youthcentrename, int role) {
+        this.id = id;
+        this.username = userName;
+        this.points = points;
+        this.fairplaypoints = fairPoints;
+        this.currentyouthcentre = youthCentre;
+        this.currentyouthcentrename = youthcentrename;
+        this.role = role;
+    }
+
+    public int getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -36,14 +45,6 @@ public class ReturnUser {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public int getPoints() {
@@ -70,59 +71,12 @@ public class ReturnUser {
         this.currentyouthcentre = currentyouthcentre;
     }
 
-    public int getRole() {
-        return role;
+    public String getCurrentyouthcentrename() {
+        return currentyouthcentrename;
     }
 
-    public void setRole(int role) {
-        this.role = role;
-    }
-
-    /*private int id;
-    private String username;
-    private int points;
-    private int fairplay_points;
-    private int current_youthcentre;
-    private int role;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getPoints() {
-        return points;
-    }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public int getFairplay_points() {
-        return fairplay_points;
-    }
-
-    public void setFairplay_points(int fairplay_points) {
-        this.fairplay_points = fairplay_points;
-    }
-
-    public int getCurrent_youthcentre() {
-        return current_youthcentre;
-    }
-
-    public void setCurrent_youthcentre(int current_youthcentre) {
-        this.current_youthcentre = current_youthcentre;
+    public void setCurrentyouthcentrename(String currentyouthcentrename) {
+        this.currentyouthcentrename = currentyouthcentrename;
     }
 
     public int getRole() {
@@ -131,5 +85,5 @@ public class ReturnUser {
 
     public void setRole(int role) {
         this.role = role;
-    }*/
+    }
 }
