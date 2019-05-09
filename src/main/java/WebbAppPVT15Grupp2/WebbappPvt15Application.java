bc6777cfea5c0a265580fa84b29a81e05117f918
CWERS <36950971+CWERS@users.noreply.github.com>
2019-05-09 22:14:14 +0200

    Skapade objekt för att ta emot fritidsgårdar från Stockholms stad samt en metod för att skapa dessa från JSON objekten.

--- src/main/java/WebbAppPVT15Grupp2/WebbappPvt15Application.java
+++ src/main/java/WebbAppPVT15Grupp2/WebbappPvt15Application.java
@@ -1,26 +1,55 @@
 package WebbAppPVT15Grupp2;
 
+import WebbAppPVT15Grupp2.Models.YouthCenter;
 import WebbAppPVT15Grupp2.Repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class WebbappPvt15Application {
 
-	public static void main(String[] args) {
-		//SpringApplication.run(WebbappPvt15Application.class, args);
+    public static void main(String[] args) {
+        //SpringApplication.run(WebbappPvt15Application.class, args);
 
-		ApplicationContext c = SpringApplication.run(WebbappPvt15Application.class,args);
+        getExternal();
 
-		UserRepository repo = c.getBean(UserRepository.class);
-		//repo.sproc_add_user("TestUser2", 1);
+        ApplicationContext c = SpringApplication.run(WebbappPvt15Application.class, args);
 
-	}
-	@RequestMapping("/")
-	String index() {
-		return "index";
-	}
+        UserRepository repo = c.getBean(UserRepository.class);
+        //repo.sproc_add_user("TestUser2", 1);
+
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
+    private static void getExternal() {
+        final String uri = "http://api.stockholm.se/ServiceGuideService/ServiceUnitTypes/af3bb542-fad5-42c5-8b8f-b9a41bbff4c2/ServiceUnits/json?apikey=56010af30b114502bfbf8db404ef41a4";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String result = restTemplate.getForObject(uri, String.class);
+        System.out.println(result);
+        ResponseEntity<List<YouthCenter>> ycResponse = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<List<YouthCenter>>() {
+        });
+
+        List<YouthCenter> list = ycResponse.getBody();
+
+        for (YouthCenter yc : list) {
+            System.out.println(yc);
+        }
+
+
+    }
 
 }
