77b705745ff4893761a926d69373caedeb2d5352
peter.froberg@stenhamra.se <jtb10lNHVJ8>
2019-05-09 09:56:39 +0200

    New update user WS

--- src/main/java/WebbAppPVT15Grupp2/Repositories/UserRepositoryImpl.java
+++ src/main/java/WebbAppPVT15Grupp2/Repositories/UserRepositoryImpl.java
@@ -84,7 +84,7 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
     }
 
     @Override
-    public List<ReturnUser> modifyUser(String id, String username, String password, String active, String points, String fairplaypoints) {
+    public List<ReturnUser> modifyUser(String id, String username, String password, String active, String points, String fairplaypoints, String ) {
         StoredProcedureQuery storedProc =
                 em.createNamedStoredProcedureQuery("sproc_update_user");
 
@@ -96,20 +96,28 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
         String sixthParam = fairplaypoints;
 
 
-        storedProc.registerStoredProcedureParameter(firstParam, String.class, ParameterMode.IN);
-        storedProc.registerStoredProcedureParameter(secondParam, String.class, ParameterMode.IN);
-        storedProc.registerStoredProcedureParameter(thirdParam, String.class, ParameterMode.IN);
-        storedProc.registerStoredProcedureParameter(fourthParam, String.class, ParameterMode.IN);
-        storedProc.registerStoredProcedureParameter(fifthParam, String.class, ParameterMode.IN);
-        storedProc.registerStoredProcedureParameter(sixthParam, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(1, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(2, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(3, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(4, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(5, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(6, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(7, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(8, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(9, String.class, ParameterMode.IN);
+        storedProc.registerStoredProcedureParameter(10, String.class, ParameterMode.IN);
 
 
-        storedProc.setParameter(firstParam, firstParam);
-        storedProc.setParameter(secondParam, secondParam);
-        storedProc.setParameter(thirdParam, thirdParam);
-        storedProc.setParameter(fourthParam, fourthParam);
-        storedProc.setParameter(fifthParam, fifthParam);
-        storedProc.setParameter(sixthParam, sixthParam);
+        storedProc.setParameter(1, id);
+        storedProc.setParameter(2, username);
+        storedProc.setParameter(3, password);
+        storedProc.setParameter(4, active);
+        storedProc.setParameter(5, points);
+        storedProc.setParameter(6, fairplaypoints);
+        storedProc.setParameter(7, );
+        storedProc.setParameter(8, fairplaypoints);
+        storedProc.setParameter(9, fairplaypoints);
+        storedProc.setParameter(10, fairplaypoints);
 
 
         return storedProc.getResultList();
