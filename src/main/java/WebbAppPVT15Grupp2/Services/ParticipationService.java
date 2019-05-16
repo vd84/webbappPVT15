bd3980db1ab08659c22d351c60b880b401f6a26b
peter.froberg@stenhamra.se <jtb10lNHVJ8>
2019-05-16 13:05:00 +0200

    Updated activity WS

--- src/main/java/WebbAppPVT15Grupp2/Services/ParticipationService.java
+++ src/main/java/WebbAppPVT15Grupp2/Services/ParticipationService.java
@@ -3,6 +3,7 @@ package WebbAppPVT15Grupp2.Services;
 import WebbAppPVT15Grupp2.Models.Participation;
 import WebbAppPVT15Grupp2.Repositories.ParticipationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,13 @@ public class ParticipationService {
 
     @RequestMapping(value = "/participation", method = RequestMethod.POST)
     public ResponseEntity<?> submitParticipation(@RequestBody Participation addParticipation, UriComponentsBuilder ucBuilder) {
-        repository.sproc_add_participation(addParticipation.getActivity(), addParticipation.getUser());
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        try {
+            repository.sproc_add_participation(addParticipation.getActivity(), addParticipation.getUser());
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataIntegrityViolationException e){
+            return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        }
+
     }
 
     @RequestMapping(value = "/participation", method = RequestMethod.GET)
